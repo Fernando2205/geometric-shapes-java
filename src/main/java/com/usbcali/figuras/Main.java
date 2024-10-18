cd90624b5ea78ae178fe67273a196cd3361c3a67
Fernando Palacios <deliopalaciosviafara@gmail.com>
2024-10-17 19:32:01 -0500

    Add controller class

--- src/main/java/com/usbcali/figuras/Main.java
+++ src/main/java/com/usbcali/figuras/Main.java
@@ -2,7 +2,7 @@ package com.usbcali.figuras;
 
 public class Main {
     public static void main(String[] args) {
-        ShapeManager shapeManager = new ShapeManager();
-        shapeManager.run();
+        Controller controller = new Controller();
+        controller.run();
     }
 }
